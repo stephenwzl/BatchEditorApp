121accc1bf28c37d8ba9357f313fa2fa03676bde
王志龙 <zhilong.wang@ele.me>
2016-04-11 16:12:53 +0800

    Initial Commit

---
+++ BatchEditor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BatchEditor
+//
+//  Created by 王志龙 on 16/4/11.
+//  Copyright © 2016年 ele.me. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ BatchEditor/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BatchEditor
+//
+//  Created by 王志龙 on 16/4/11.
+//  Copyright © 2016年 ele.me. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
