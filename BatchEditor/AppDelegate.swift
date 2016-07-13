5d14fb426062139a574b005836bac5d11a958d6e
王志龙 <zhilong.wang@ele.me>
2016-07-13 13:41:31 +0800

    Initial Commit

---
+++ BatchEditor/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BatchEditor
+//
+//  Created by 王志龙 on 16/7/13.
+//  Copyright © 2016年 stephenw.cc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
